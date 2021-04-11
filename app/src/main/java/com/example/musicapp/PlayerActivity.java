3431f7b4732a57a573cd58d9275bba42a6e126c9
bichquyen <bichquyenpy@gmail.com>
2021-04-11 16:58:14 +0700

    Hien UI

---
+++ app/src/main/java/com/example/musicapp/PlayerActivity.java
@@ -0,0 +1,20 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+
+    public void playpausebutton(View view) {
+    }
+
+
+}
